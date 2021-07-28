3d190c19e29226e1828412f6f59485c8ca14bcfd
RaghadEstanbouli <Raghad.Estanbouli@flemingcollege.ca>
2021-07-28 16:22:36 -0400

    Update config.js

--- map/config.js
+++ map/config.js
@@ -25,7 +25,7 @@ var config = {
       image: './Images/Screenshot-01.png',
       description: 'Desire to learn by focusing on every project detail to develop a career in GIS',
       location: {
-        center: { lon: -27.67905, lat: 41.85173 },
+        center: { lon: -42.53906, lat: 11.17840 },
         zoom: 1.59,
         pitch: 0.00,
         bearing: 0.00
@@ -194,7 +194,7 @@ var config = {
       hidden: false,
       image: './Images/Screenshot-02.png',
       location: { 
-        center: { lon: -27.67905, lat: 41.85173 },
+        center: { lon: -42.53906, lat: 11.17840 },
         zoom: 1.59,
         pitch: 0.00,
         bearing: 0.00
