421119bd4ee3355a2618e9def7cb9cdbe47b3c7a
Raghad Estanbouli <Raghad.Estanbouli@flemingcollege.ca>
2021-08-03 11:29:16 -0400

    ff

--- JS.js
+++ JS.js
@@ -36,9 +36,7 @@ function myFunction() {
 }
 //https://www.w3schools.com/js/js_events.asp
 // code for button on Contact Us page submit popup
-function myFunctionpop() {
-  alert("Thank you");
-}
+
 
 
 function changearrow() {
